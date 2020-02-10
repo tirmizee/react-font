76c248cea5558434f7c12aba7d8d9797f2370801
Pratya Yeekhaday <pratya.y@gdl-technology.com>
2020-02-10 17:34:23 +0700

    add views

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import Header from './containers/DefualtLayout/Header'
 import Menu from './containers/DefualtLayout/Menu'
 import Footer from './containers/DefualtLayout/Footer'
@@ -8,10 +9,12 @@ export default class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header/>
-        <Menu/>
-        <Content/>
-        <Footer/> 
+        <BrowserRouter>
+          <Header/>
+          <Menu/>
+          <Content/>
+          <Footer/> 
+        </BrowserRouter>
       </div>
     )
   }
